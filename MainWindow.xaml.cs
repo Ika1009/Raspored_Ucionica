426beaa35003ae8c667b96d2d8b9f2ed52e04d5e
Dzekanaa <pesicnikola50@gmail.com>
2022-10-02 22:24:42 +0200

    tabele su napravljenje sve

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,9 +26,9 @@ namespace Raspored_Ucionica
             InitializeComponent();
             DataTable dt = new DataTable();
             DataTable dt2 = new DataTable();
-           /* DataTable dt3 = new DataTable();
+            DataTable dt3 = new DataTable();
             DataTable dt4 = new DataTable();
-            DataTable dt5 = new DataTable();*/
+            DataTable dt5 = new DataTable();
             int nbColumns = 32;
             int nbRows = 8;
             MainPageViewModel viewModel = new MainPageViewModel();
@@ -37,9 +37,9 @@ namespace Raspored_Ucionica
             {
                 dt.Columns.Add((i/8+1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));
                 dt2.Columns.Add((i / 8 + 1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));
-                /*dt3.Columns.Add((i / 8 + 1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));
+                dt3.Columns.Add((i / 8 + 1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));
                 dt4.Columns.Add((i / 8 + 1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));
-                dt5.Columns.Add((i / 8 + 1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));*/
+                dt5.Columns.Add((i / 8 + 1).ToString() + "-" + (i % 8 + 1).ToString(), typeof(string));
             }
             
 
@@ -48,30 +48,30 @@ namespace Raspored_Ucionica
                 //dt.Rows.Add(row.ToString() + ".cas");
                 DataRow dr2 = dt2.NewRow();
                 DataRow dr = dt.NewRow();
-                /*DataRow dr3 = dt3.NewRow();
-                DataRow dr4 = dt.NewRow();
-                DataRow dr5 = dt2.NewRow();*/
+                DataRow dr3 = dt3.NewRow();
+                DataRow dr4 = dt4.NewRow();
+                DataRow dr5 = dt5.NewRow();
                 for (int col = 0; col < nbColumns; col++)
                 {
                     
                     dr[col] = viewModel.rezultatiPonedeljak[row][col];
                     dr2[col] = viewModel.rezultatiUtorak[row][col];
-                    /*dr3[col] = viewModel.rezultatiSreda[row][col];
+                    dr3[col] = viewModel.rezultatiSreda[row][col];
                     dr4[col] = viewModel.rezultatiCetvrtak[row][col];
-                    dr5[col] = viewModel.rezultatiPetak[row][col];*/
+                    dr5[col] = viewModel.rezultatiPetak[row][col];
                 }
                 dt.Rows.Add(dr);
                 dt2.Rows.Add(dr2);
-                /*dt3.Rows.Add(dr3);
+                dt3.Rows.Add(dr3);
                 dt4.Rows.Add(dr4);
-                dt5.Rows.Add(dr5);*/
+                dt5.Rows.Add(dr5);
             }
 
             rezultatiPon.ItemsSource = dt.DefaultView;
             rezultatiUto.ItemsSource = dt2.DefaultView;
-           /* rezultatiSre.ItemsSource = dt3.DefaultView;
+            rezultatiSre.ItemsSource = dt3.DefaultView;
             rezultatiCet.ItemsSource = dt4.DefaultView;
-            rezultatiPet.ItemsSource = dt5.DefaultView;*/
+            rezultatiPet.ItemsSource = dt5.DefaultView;
         }
 
     }
