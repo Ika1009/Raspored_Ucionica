90419466593b41157ad332415992c47c26ce4bab
Dzekanaa <pesicnikola50@gmail.com>
2022-11-07 13:01:12 +0100

    sad radi samo za lutajuca

--- View/InputWindow.xaml.cs
+++ View/InputWindow.xaml.cs
@@ -224,39 +224,63 @@ namespace Raspored_Ucionica
             {
                 case "checked11":
                     viewModel.Checked11 = false;
+                    textbox11.Text = "";
+                    textbox11.IsEnabled = true;
                     break;
                 case "checked12":
                     viewModel.Checked12 = false;
+                    textbox12.Text = "";
+                    textbox12.IsEnabled = true;
                     break;
                 case "checked13":
                     viewModel.Checked13 = false;
+                    textbox13.Text = "";
+                    textbox13.IsEnabled = true;
                     break;
                 case "checked21":
                     viewModel.Checked21 = false;
+                    textbox21.Text = "";
+                    textbox21.IsEnabled = true;
                     break;
                 case "checked22":
                     viewModel.Checked22 = false;
+                    textbox22.Text = "";
+                    textbox22.IsEnabled = true;
                     break;
                 case "checked23":
                     viewModel.Checked23 = false;
+                    textbox32.Text = "";
+                    textbox32.IsEnabled = true;
                     break;
                 case "checked31":
                     viewModel.Checked31 = false;
+                    textbox31.Text = "";
+                    textbox31.IsEnabled = true;
                     break;
                 case "checked32":
                     viewModel.Checked32 = false;
+                    textbox32.Text = "";
+                    textbox32.IsEnabled = true;
                     break;
                 case "checked33":
                     viewModel.Checked33 = false;
+                    textbox33.Text = "";
+                    textbox33.IsEnabled = true;
                     break;
                 case "checked41":
                     viewModel.Checked41 = false;
+                    textbox41.Text = "";
+                    textbox41.IsEnabled = true;
                     break;
                 case "checked42":
                     viewModel.Checked42 = false;
+                    textbox42.Text = "";
+                    textbox42.IsEnabled = true;
                     break;
                 case "checked43":
                     viewModel.Checked43 = false;
+                    textbox43.Text = "";
+                    textbox43.IsEnabled = true;
                     break;
                 default:
                     MessageBox.Show("Nesto je poslo opasno naopako");
